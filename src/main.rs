5f6aec5b6cc8d7ee307d1bbe4054c3d6cd194b4b
ingobeans <127751491+ingobeans@users.noreply.github.com>
2025-07-17 01:15:37 +0200

    make tiles "expandable"

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,14 @@ enum TileData {
 }
 
 struct Minefield {
-    mines: Vec<Vec<(Tile, TileData)>>,
+    field: Vec<Vec<(Tile, TileData)>>,
+    size: usize,
 }
 impl Minefield {
     fn empty(size: usize) -> Self {
         let row = vec![(Tile::Clear(0), TileData::Unknown); size];
         let field = vec![row; size];
-        Self { mines: field }
+        Self { field, size }
     }
     /// Generate a new minefield "around" a mouse click,
     /// ensuring that the clicked tile is safe, with a value of 0
@@ -31,21 +32,26 @@ impl Minefield {
         // Keep regenerating minefields until one fills the conditions
         loop {
             let mut field = Self::empty(size);
+            let mut remaining_mines = mines_amount;
 
-            for _ in 0..mines_amount {
+            while remaining_mines > 0 {
                 let x = rng.gen_range(0, size);
                 let y = rng.gen_range(0, size);
-                field.mines[x][y].0 = Tile::Mine;
+                if let Tile::Mine = field.field[x][y].0 {
+                    continue;
+                }
+                remaining_mines -= 1;
+                field.field[x][y].0 = Tile::Mine;
                 // update neighbours
                 for neighbour_y in y.saturating_sub(1)..(y + 2).min(size) {
                     for neighbour_x in x.saturating_sub(1)..(x + 2).min(size) {
-                        if let Tile::Clear(value) = &mut field.mines[neighbour_x][neighbour_y].0 {
+                        if let Tile::Clear(value) = &mut field.field[neighbour_x][neighbour_y].0 {
                             *value += 1;
                         }
                     }
                 }
             }
-            if let Tile::Clear(value) = field.mines[click_x][click_y].0 {
+            if let Tile::Clear(value) = field.field[click_x][click_y].0 {
                 if value == 0 {
                     return field;
                 }
@@ -53,7 +59,7 @@ impl Minefield {
         }
     }
     fn draw(&self, scaling: f32, offset_x: f32, offset_y: f32) {
-        for (x, column) in self.mines.iter().enumerate() {
+        for (x, column) in self.field.iter().enumerate() {
             for (y, tile) in column.iter().enumerate() {
                 let tile_x = x as f32 * scaling + offset_x;
                 let tile_y = y as f32 * scaling + offset_y;
@@ -108,6 +114,58 @@ impl Minefield {
             }
         }
     }
+    /// Handles a click on the minefield. If clicked tile is unknown, it is revealed.
+    /// If it is known, it is expanded
+    fn handle_click(&mut self, x: usize, y: usize) {
+        let tile_data = self.field[x][y].1;
+        match tile_data {
+            // if tile is unknown, make it known
+            TileData::Unknown => {
+                self.reveal_tile(x, y);
+            }
+            TileData::Known => {
+                self.try_expand_tile(x, y);
+            }
+            _ => {}
+        }
+    }
+    /// Reveals a tile
+    fn reveal_tile(&mut self, x: usize, y: usize) {
+        let tile = &mut self.field[x][y];
+        if let Tile::Mine = tile.0 {
+            println!("game over!!");
+        }
+        tile.1 = TileData::Known;
+    }
+    /// "Expands" a tile at position, i.e. clicking its neighbouring tiles that arent flagged.
+    /// If too few of its neighbours are known, it does nothing
+    fn try_expand_tile(&mut self, x: usize, y: usize) {
+        let tile = self.field[x][y].0;
+        if let Tile::Clear(value) = tile {
+            // count unknown neighbours and flagged neighbours
+            let mut flagged_neighbours = 0;
+            let mut unknown_neighbours = Vec::new();
+
+            for neighbour_y in y.saturating_sub(1)..(y + 2).min(self.size) {
+                for neighbour_x in x.saturating_sub(1)..(x + 2).min(self.size) {
+                    match self.field[neighbour_x][neighbour_y].1 {
+                        TileData::Unknown => {
+                            unknown_neighbours.push((neighbour_x, neighbour_y));
+                        }
+                        TileData::Flagged => {
+                            flagged_neighbours += 1;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            if flagged_neighbours >= value {
+                for (x, y) in unknown_neighbours {
+                    self.reveal_tile(x, y)
+                }
+            }
+        }
+    }
 }
 
 const TILE_VALUE_COLORS: [Color; 8] = [BLUE, GREEN, RED, PURPLE, RED, ORANGE, YELLOW, WHITE];
@@ -155,10 +213,11 @@ async fn main() {
                 );
                 first_click = false;
             }
-            minefield.mines[mouse_tile_x][mouse_tile_y].1 = TileData::Known;
+
+            minefield.handle_click(mouse_tile_x, mouse_tile_y);
         }
         if is_mouse_button_pressed(MouseButton::Right) && mouse_tile_in_bounds {
-            let tile = &mut minefield.mines[mouse_tile_x][mouse_tile_y].1;
+            let tile = &mut minefield.field[mouse_tile_x][mouse_tile_y].1;
             match tile {
                 TileData::Flagged => {
                     *tile = TileData::Unknown;
